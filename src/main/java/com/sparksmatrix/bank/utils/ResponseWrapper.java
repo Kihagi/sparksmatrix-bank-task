6b456cc1d51d3804cf7a0c4f10106b748cb86c72
Mathenge <anthonykihagi@gmail.com>
2025-01-25 18:51:44 +0300

    Add Swagger

--- src/main/java/com/sparksmatrix/bank/utils/ResponseWrapper.java
+++ src/main/java/com/sparksmatrix/bank/utils/ResponseWrapper.java
@@ -12,16 +12,4 @@ public class ResponseWrapper<T> {
     private final int code;
     private final String message;
     private T data;
-
-    /**
-     * Check if the transaction is successful
-     *
-     * @return
-     */
-    public boolean isSuccessful() {
-        if ((this.code == HttpStatus.OK.value()) || (this.code == HttpStatus.CREATED.value())) {
-            return true;
-        }
-        return false;
-    }
 }
